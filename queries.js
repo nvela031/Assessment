cfa66f51a66f96947f78068ee1f2bf4dbbe77d99
nvela031 <nvela031@fiu.edu>
2021-01-24 14:18:30 -0500

    make the captcha validation to work and make the insert query to work as well

--- queries.js
+++ queries.js
@@ -1,6 +1,7 @@
 const request = require("request");
-
+const fetch = require("node-fetch");
 const Pool = require("pg").Pool;
+
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
@@ -19,15 +20,17 @@ const getStudents = (req, res) => {
 };
 
 const createStudent = (req, res) => {
-  const { firstname, lastname } = req.body;
+  const { firstname, lastname, captcha } = req.body;
 
-  // if (
-  //   req.body.captcha === undefined ||
-  //   req.body.captcah === "" ||
-  //   req.body.captcah == null
-  // ) {
-  //   return res.json({ success: false, msg: "Please select captcha" });
-  // }
+  if (captcha === undefined || captcha === "" || captcha == null) {
+    return res.json({ success: false, msg: "Please select captcha" });
+  }
+
+  const isValid = validation(captcha, req.connection.remoteAddress);
+
+  if (isValid.success === false) {
+    return res.status(400).json({ msg: "Failed captcha verification" });
+  }
 
   pool.query(
     "INSERT INTO student (firstname, lastname) VALUES ($1, $2)",
@@ -36,12 +39,18 @@ const createStudent = (req, res) => {
       if (error) {
         throw error;
       }
-      
       res.status(200).json(results);
     }
   );
+};
 
-    
+const validation = async (captcha, remoteAddress) => {
+  const secretKey = "6Le6KjkaAAAAADamxfClznJWPSntLcJYKtivfx_x";
+  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captcha}&remoteip=${remoteAddress}`;
+
+  return await fetch(verifyUrl)
+    .then((res) => res.json())
+    .then((data) => console.log(data.success));
 };
 
 module.exports = {
